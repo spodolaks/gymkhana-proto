02221d7fef35c810a3ee93abb9af022fc2808ef7
Sergejs Podolaks <sergejspodolaks@Sergejss-MacBook-Pro-2.local>
2023-12-11 18:34:09 +0200

    fix proto file generation

--- gen/v1/go/gymkhana/competition_grpc.pb.go
+++ gen/v1/go/gymkhana/competition_grpc.pb.go
@@ -23,8 +23,8 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type CompetitionServiceClient interface {
 	Competition(ctx context.Context, in *CompetitionRequest, opts ...grpc.CallOption) (*CompetitionResponse, error)
-	CompetitionStages(ctx context.Context, in *CompetitionRequest, opts ...grpc.CallOption) (CompetitionService_CompetitionStagesClient, error)
-	ListCompetitions(ctx context.Context, in *CompetitionListRequest, opts ...grpc.CallOption) (CompetitionService_ListCompetitionsClient, error)
+	CompetitionStages(ctx context.Context, in *CompetitionRequest, opts ...grpc.CallOption) (*CompetitionListResponse, error)
+	ListCompetitions(ctx context.Context, in *CompetitionListRequest, opts ...grpc.CallOption) (*CompetitionListResponse, error)
 	CreateCompetition(ctx context.Context, in *CompetitionCreateRequest, opts ...grpc.CallOption) (*CompetitionResponse, error)
 	UpdateCompetition(ctx context.Context, in *CompetitionUpdateRequest, opts ...grpc.CallOption) (*CompetitionResponse, error)
 	DeleteCompetition(ctx context.Context, in *CompetitionRequest, opts ...grpc.CallOption) (*CompetitionResponse, error)
@@ -47,68 +47,22 @@ func (c *competitionServiceClient) Competition(ctx context.Context, in *Competit
 	return out, nil
 }
 
-func (c *competitionServiceClient) CompetitionStages(ctx context.Context, in *CompetitionRequest, opts ...grpc.CallOption) (CompetitionService_CompetitionStagesClient, error) {
-	stream, err := c.cc.NewStream(ctx, &CompetitionService_ServiceDesc.Streams[0], "/gymkhana.CompetitionService/CompetitionStages", opts...)
+func (c *competitionServiceClient) CompetitionStages(ctx context.Context, in *CompetitionRequest, opts ...grpc.CallOption) (*CompetitionListResponse, error) {
+	out := new(CompetitionListResponse)
+	err := c.cc.Invoke(ctx, "/gymkhana.CompetitionService/CompetitionStages", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &competitionServiceCompetitionStagesClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
+	return out, nil
 }
 
-type CompetitionService_CompetitionStagesClient interface {
-	Recv() (*CompetitionStage, error)
-	grpc.ClientStream
-}
-
-type competitionServiceCompetitionStagesClient struct {
-	grpc.ClientStream
-}
-
-func (x *competitionServiceCompetitionStagesClient) Recv() (*CompetitionStage, error) {
-	m := new(CompetitionStage)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func (c *competitionServiceClient) ListCompetitions(ctx context.Context, in *CompetitionListRequest, opts ...grpc.CallOption) (CompetitionService_ListCompetitionsClient, error) {
-	stream, err := c.cc.NewStream(ctx, &CompetitionService_ServiceDesc.Streams[1], "/gymkhana.CompetitionService/ListCompetitions", opts...)
+func (c *competitionServiceClient) ListCompetitions(ctx context.Context, in *CompetitionListRequest, opts ...grpc.CallOption) (*CompetitionListResponse, error) {
+	out := new(CompetitionListResponse)
+	err := c.cc.Invoke(ctx, "/gymkhana.CompetitionService/ListCompetitions", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
-	x := &competitionServiceListCompetitionsClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-type CompetitionService_ListCompetitionsClient interface {
-	Recv() (*CompetitionResponse, error)
-	grpc.ClientStream
-}
-
-type competitionServiceListCompetitionsClient struct {
-	grpc.ClientStream
-}
-
-func (x *competitionServiceListCompetitionsClient) Recv() (*CompetitionResponse, error) {
-	m := new(CompetitionResponse)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
+	return out, nil
 }
 
 func (c *competitionServiceClient) CreateCompetition(ctx context.Context, in *CompetitionCreateRequest, opts ...grpc.CallOption) (*CompetitionResponse, error) {
@@ -143,8 +97,8 @@ func (c *competitionServiceClient) DeleteCompetition(ctx context.Context, in *Co
 // for forward compatibility
 type CompetitionServiceServer interface {
 	Competition(context.Context, *CompetitionRequest) (*CompetitionResponse, error)
-	CompetitionStages(*CompetitionRequest, CompetitionService_CompetitionStagesServer) error
-	ListCompetitions(*CompetitionListRequest, CompetitionService_ListCompetitionsServer) error
+	CompetitionStages(context.Context, *CompetitionRequest) (*CompetitionListResponse, error)
+	ListCompetitions(context.Context, *CompetitionListRequest) (*CompetitionListResponse, error)
 	CreateCompetition(context.Context, *CompetitionCreateRequest) (*CompetitionResponse, error)
 	UpdateCompetition(context.Context, *CompetitionUpdateRequest) (*CompetitionResponse, error)
 	DeleteCompetition(context.Context, *CompetitionRequest) (*CompetitionResponse, error)
@@ -158,11 +112,11 @@ type UnimplementedCompetitionServiceServer struct {
 func (UnimplementedCompetitionServiceServer) Competition(context.Context, *CompetitionRequest) (*CompetitionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Competition not implemented")
 }
-func (UnimplementedCompetitionServiceServer) CompetitionStages(*CompetitionRequest, CompetitionService_CompetitionStagesServer) error {
-	return status.Errorf(codes.Unimplemented, "method CompetitionStages not implemented")
+func (UnimplementedCompetitionServiceServer) CompetitionStages(context.Context, *CompetitionRequest) (*CompetitionListResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CompetitionStages not implemented")
 }
-func (UnimplementedCompetitionServiceServer) ListCompetitions(*CompetitionListRequest, CompetitionService_ListCompetitionsServer) error {
-	return status.Errorf(codes.Unimplemented, "method ListCompetitions not implemented")
+func (UnimplementedCompetitionServiceServer) ListCompetitions(context.Context, *CompetitionListRequest) (*CompetitionListResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListCompetitions not implemented")
 }
 func (UnimplementedCompetitionServiceServer) CreateCompetition(context.Context, *CompetitionCreateRequest) (*CompetitionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateCompetition not implemented")
@@ -204,46 +158,40 @@ func _CompetitionService_Competition_Handler(srv interface{}, ctx context.Contex
 	return interceptor(ctx, in, info, handler)
 }
 
-func _CompetitionService_CompetitionStages_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(CompetitionRequest)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
+func _CompetitionService_CompetitionStages_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CompetitionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
 	}
-	return srv.(CompetitionServiceServer).CompetitionStages(m, &competitionServiceCompetitionStagesServer{stream})
-}
-
-type CompetitionService_CompetitionStagesServer interface {
-	Send(*CompetitionStage) error
-	grpc.ServerStream
-}
-
-type competitionServiceCompetitionStagesServer struct {
-	grpc.ServerStream
-}
-
-func (x *competitionServiceCompetitionStagesServer) Send(m *CompetitionStage) error {
-	return x.ServerStream.SendMsg(m)
-}
-
-func _CompetitionService_ListCompetitions_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(CompetitionListRequest)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
+	if interceptor == nil {
+		return srv.(CompetitionServiceServer).CompetitionStages(ctx, in)
 	}
-	return srv.(CompetitionServiceServer).ListCompetitions(m, &competitionServiceListCompetitionsServer{stream})
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gymkhana.CompetitionService/CompetitionStages",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CompetitionServiceServer).CompetitionStages(ctx, req.(*CompetitionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
-type CompetitionService_ListCompetitionsServer interface {
-	Send(*CompetitionResponse) error
-	grpc.ServerStream
-}
-
-type competitionServiceListCompetitionsServer struct {
-	grpc.ServerStream
-}
-
-func (x *competitionServiceListCompetitionsServer) Send(m *CompetitionResponse) error {
-	return x.ServerStream.SendMsg(m)
+func _CompetitionService_ListCompetitions_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CompetitionListRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CompetitionServiceServer).ListCompetitions(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gymkhana.CompetitionService/ListCompetitions",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CompetitionServiceServer).ListCompetitions(ctx, req.(*CompetitionListRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _CompetitionService_CreateCompetition_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -312,6 +260,14 @@ var CompetitionService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _CompetitionService_Competition_Handler,
 		},
 		{
+			MethodName: "CompetitionStages",
+			Handler:    _CompetitionService_CompetitionStages_Handler,
+		},
+		{
+			MethodName: "ListCompetitions",
+			Handler:    _CompetitionService_ListCompetitions_Handler,
+		},
+		{
 			MethodName: "CreateCompetition",
 			Handler:    _CompetitionService_CreateCompetition_Handler,
 		},
@@ -324,17 +280,6 @@ var CompetitionService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _CompetitionService_DeleteCompetition_Handler,
 		},
 	},
-	Streams: []grpc.StreamDesc{
-		{
-			StreamName:    "CompetitionStages",
-			Handler:       _CompetitionService_CompetitionStages_Handler,
-			ServerStreams: true,
-		},
-		{
-			StreamName:    "ListCompetitions",
-			Handler:       _CompetitionService_ListCompetitions_Handler,
-			ServerStreams: true,
-		},
-	},
+	Streams:  []grpc.StreamDesc{},
 	Metadata: "gymkhana/competition.proto",
 }
